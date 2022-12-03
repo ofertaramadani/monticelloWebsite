08b8f5df1983e7386b58e17f292925aa1a638215
ofertaramadani <oferte.ramadani@student.uni-pr.edu>
2022-12-04 00:19:13 +0100

    updates

--- main.js
+++ main.js
@@ -15,19 +15,38 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
-$('.owl-hero').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:3
-        },
-        600:{
-            items:3      
-         },
-        1000:{
-            items:3
-        }
-    }
+// $('.owl-hero').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:3
+//         },
+//         600:{
+//             items:3      
+//          },
+//         1000:{
+//             items:3
+//         }
+//     }
+// })
+
+// const links = document.querySelectorAll('.scroll_to');
+// function r(){
+// links.forEach((item) =>{
+//     item.addEventListener('click',()=>{
+//         const el=document.getElementById(item.getAttribute('data-link'));
+//         el.current.scrollIntoView({ behavior : 'smooth'});
+//     });
+// });
+// }
+// $( document ).ready(r) ;
+
+$(document).ready(function a(){
+    document.getElementById('section1').scrollIntoView({behavior: "smooth"});
 })
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+});
