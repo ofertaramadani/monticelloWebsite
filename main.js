78d9c6ac91c252f31a11ecd56614a3696c06b804
ofertaramadani <oferte.ramadani@student.uni-pr.edu>
2022-12-04 21:30:09 +0100

    fixed hamburgering

--- main.js
+++ main.js
@@ -42,12 +42,16 @@ $('.owl-two').owlCarousel({
     }
 })
 
-$(".hamburger").click(function(){
+$(".nav-toggle").click(function(){
     $(".hamburgerCLICK").toggle("slow");
   });
-$(".foto").click(function(){
-    var src=document.getElementById('foto1').src;
-    var img = document.createElement('img');
-    img.src=src;
-    document.getElementById('body').appendChild = img;
-});
+
+let navToggle = document.querySelector('.nav-toggle')
+let bars = document.querySelectorAll('.bar')
+
+function toggleHamburger(e) {
+  bars.forEach(bar => bar.classList.toggle('x'))
+}
+
+navToggle.addEventListener('click', toggleHamburger)
+
